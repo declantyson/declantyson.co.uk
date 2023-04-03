e3917ebe5bf3c79f9b5326cd0da01a775cf0f3c9
declantyson <dec@declantyson.co.uk>
2023-04-03 16:59:56 +0100

    hide blog, update pbh image

--- server.js
+++ server.js
@@ -51,62 +51,62 @@ app.get('/', function(req,res) {
     });
 });
 
-app.get('/blog/:slug', function(req,res) {
-    let md = new MarkdownIt(),
-        file = `views/content/blogs/${req.params.slug}.md`;
+// app.get('/blog/:slug', function(req,res) {
+//     let md = new MarkdownIt(),
+//         file = `views/content/blogs/${req.params.slug}.md`;
 
-    fs.readFile(file, 'utf-8', (err, content) => {
-        if (err) {
-            console.error(err);
-            content = "# 404 \n ###### Page not found \n Sorry about that. Please check out the rest of the site!"
-            res.status(404);
-        }
-        res.render("view-blog", {
-            'package': package,
-            'scripts': getScripts(),
-            'content': md.render(content)
-        });
-    });
-});
+//     fs.readFile(file, 'utf-8', (err, content) => {
+//         if (err) {
+//             console.error(err);
+//             content = "# 404 \n ###### Page not found \n Sorry about that. Please check out the rest of the site!"
+//             res.status(404);
+//         }
+//         res.render("view-blog", {
+//             'package': package,
+//             'scripts': getScripts(),
+//             'content': md.render(content)
+//         });
+//     });
+// });
 
-app.get('/blog', function(req,res) {
-    let blogs = [];
+// app.get('/blog', function(req,res) {
+//     let blogs = [];
 
-    fs.readdir('views/content/blogs/', (err, files) => {
-       if(err) throw err;
-       files.forEach((file) => {
-           let md = new MarkdownIt(),
-               content = fs.readFileSync(`views/content/blogs/${file}`, 'utf-8');
+//     fs.readdir('views/content/blogs/', (err, files) => {
+//        if(err) throw err;
+//        files.forEach((file) => {
+//            let md = new MarkdownIt(),
+//                content = fs.readFileSync(`views/content/blogs/${file}`, 'utf-8');
 
-           let rendered = md.render(content),
-               titleStartIndex = rendered.indexOf("<h1>") + 4,
-               dateStartIndex = rendered.indexOf("<h4>") + 4,
-               titleEndIndex = rendered.indexOf("</h1>") ,
-               dateEndIndex = rendered.indexOf("</h4>") ,
-               blogTitle = rendered.substring(titleStartIndex, titleEndIndex),
-               blogDate = rendered.substring(dateStartIndex, dateEndIndex),
-               blog = {
-                   url: `/blog/${file.replace('.md', '')}`,
-                   title: blogTitle,
-                   date: blogDate
-               };
+//            let rendered = md.render(content),
+//                titleStartIndex = rendered.indexOf("<h1>") + 4,
+//                dateStartIndex = rendered.indexOf("<h4>") + 4,
+//                titleEndIndex = rendered.indexOf("</h1>") ,
+//                dateEndIndex = rendered.indexOf("</h4>") ,
+//                blogTitle = rendered.substring(titleStartIndex, titleEndIndex),
+//                blogDate = rendered.substring(dateStartIndex, dateEndIndex),
+//                blog = {
+//                    url: `/blog/${file.replace('.md', '')}`,
+//                    title: blogTitle,
+//                    date: blogDate
+//                };
 
-           blogs.push(blog);
-       });
+//            blogs.push(blog);
+//        });
 
-       blogs = blogs.sort(function(a,b){
-           a = Date.parse(a.date.replace(/(\d{1,2})[a-z]{2}\b/i, ''));
-           b = Date.parse(b.date.replace(/(\d{1,2})[a-z]{2}\b/i, ''));
-           return a>b ? -1 : a<b ? 1 : 0;
-       });
+//        blogs = blogs.sort(function(a,b){
+//            a = Date.parse(a.date.replace(/(\d{1,2})[a-z]{2}\b/i, ''));
+//            b = Date.parse(b.date.replace(/(\d{1,2})[a-z]{2}\b/i, ''));
+//            return a>b ? -1 : a<b ? 1 : 0;
+//        });
 
-        res.render('blog', {
-            'package': package,
-            'scripts': getScripts(),
-            'blogs' : blogs
-        });
-    });
-});
+//         res.render('blog', {
+//             'package': package,
+//             'scripts': getScripts(),
+//             'blogs' : blogs
+//         });
+//     });
+// });
 
 app.get('/portfolio', function(req,res) {
     let work = [],
